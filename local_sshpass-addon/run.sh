9c2c2af377126bf7161ac4f627392c18c2db50d5
bsimson69 <100311555+bsimson69@users.noreply.github.com>
2025-03-16 00:12:32 +0100

    Create run.sh

---
+++ local_sshpass-addon/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "SSHpass Add-on is running. SSHpass and openssh-client are available."
+# Halte den Container am Laufen, damit er Befehle ausführen kann, wenn benötigt.
+tail -f /dev/null
