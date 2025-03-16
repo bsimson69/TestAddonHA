27c8bae4a2e963dd9dd1cd7a6caacd275df519c5
bsimson69 <100311555+bsimson69@users.noreply.github.com>
2025-03-16 01:23:37 +0100

    Update run.sh

--- local_sshpass-addon/run.sh
+++ local_sshpass-addon/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-echo "Starte MQTT-Trigger..."
-exec python3 /trigger.py
+echo "SSHpass Add-on is running. SSHpass and openssh-client are available."
+# Halte den Container am Laufen, damit er Befehle ausführen kann, wenn benötigt.
+tail -f /dev/null
